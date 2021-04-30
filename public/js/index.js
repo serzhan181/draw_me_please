73af1c5833c405416de0c6a25951b1ff7e0866ff
lektorcode <lektorcode@yandex.com>
2021-04-30 14:50:16 +0600

    vanilla js setup

--- public/js/index.js
+++
@@ -1,8 +0,0 @@
-import { Brush } from './tools/brush.js'
-const canvas = document.querySelector('canvas')
-canvas.width = innerWidth - 50
-canvas.height = innerHeight - 50
-
-document.addEventListener('DOMContentLoaded', () => {
-  new Brush(canvas)
-})
